a5f204d946daae7cb18ea46468072b7e66b037b6
Sabbir Ahmed <sabbir.ove@gmail.com>
2020-06-20 11:45:10 +0600

    Beach_Resort_PagesAdded

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class single extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
