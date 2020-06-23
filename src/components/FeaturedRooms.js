7e22640b8fe6c95318fce0b68a7f20a1912b34dc
Sabbir Ahmed <sabbir.ove@gmail.com>
2020-06-23 21:27:19 +0600

    FormattedDataAdded

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {RoomContext} from '../context';
+export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
+
+    render() {
+        const value = this.context;
+        return (
+            <div>
+                hellof {value}
+            </div>
+        )
+    }
+}
---
+++ src/components/Services.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import Title from './Title';
+import {FaCocktail,FaHiking,FaShuttleVan,FaBeer} from 'react-icons';
+export default class Services extends Component {
+    
+    state=[
+        {
+        icon:<FaCocktail/>,
+        title:"free cocktails",
+        info:'Free cockatails is hard to come by so if . . .'
+    },
+    {
+        icon:<FaHiking/>,
+        title:"free Hiking",
+        info:'Free cockatails is hard to come by so if . . .'
+
+    },
+    {
+        icon:<FaShuttleVan/>,
+        title:"free van",
+        info:'Free cockatails is hard to come by so if . . .'
+
+    }
+    ,
+    {
+        icon:<FaBeer/>,
+        title:"free Beer",
+        info:'Free cockatails is hard to come by so if . . .'
+
+    }
+
+]
+    render() {
+        return (
+            <div className="services">
+                <Title title='Services'/>
+                <div className="services-center">
+                     {this.state.map((item,index)=>{
+return <article key={index} className="service">
+    <span>{item.icon}</span>
+                     <h6>{item.title}</h6>
+                     <p>{item.info}</p>
+</article>
+                     })}
+                </div>
+            </div>
+        )
+    }
+}
