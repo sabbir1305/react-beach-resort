96a43d27cf4e48832cfea7cd824b498823a66dcd
Sabbir Ahmed <sabbir.ove@gmail.com>
2020-06-26 18:53:59 +0600

    LoadingAndRoombackComponentAdded

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import {RoomContext} from '../context';
+import Loading from './Loading';
+import Room from './Room';
+import Title from './Title';
+
 export default class FeaturedRooms extends Component {
     static contextType = RoomContext;
 
     render() {
-        const value = this.context;
+        let {loading , featuredRooms: rooms} = this.context;
+       
+       rooms = rooms.map(room=>{
+           return <Room key={room.id} room = {room} />
+          
+       })
+       
         return (
-            <div>
-                hellof {value}
-            </div>
+            <section className="featured-rroms">
+                <Title title="featured rooms"/>
+
+                <div className="featured-rooms-center">
+                    {loading?<Loading/>:rooms}
+                </div>
+                
+            </section>
         )
     }
 }
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Title from './Title';
-import {FaCocktail,FaHiking,FaShuttleVan,FaBeer} from 'react-icons';
+import {FaCocktail,FaHiking,FaShuttleVan,FaBeer} from 'react-icons/fa';
 export default class Services extends Component {
     
     state=[
