4edb60cdedb79810538103e9c7e302bfbca847af
Sabbir Ahmed <sabbir.ove@gmail.com>
2020-07-03 19:03:39 +0600

    SingleRoomUpdated

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,51 @@
 import React, { Component } from 'react'
+import defaultBcg from '../images/room-10.jpeg';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import {RoomContext} from '../context';
+
+
 
 export default class SingleRoom extends Component {
+constructor(props){
+    super(props);
+
+    this.state={
+        slug:this.props.match.params.slug,
+        defaultBcg
+    }
+}
+
+static contextType = RoomContext;
+
+// componentDidMount(){
+
+// }
+
     render() {
-        return (
-            <div>
-                sd
+
+        const {getRoom} = this.context;
+        const room = getRoom(this.state.slug);
+        if(!room){
+            return <div className="error">
+                    <h3>no such room colud be found</h3>
+            
+            <Link to="/rooms" className="btn-primary">
+                back to rooms
+            </Link>
             </div>
+        }
+
+        const {name,description,capacity,size,price,extras,breakfast,pets,images}=room;
+        return (
+            <Hero hero="roomsHero">
+                <Banner title={`${name} room`}>
+                    <Link to='/rooms' className="btn-primary">
+                        back to rooms
+                    </Link>
+                </Banner>
+            </Hero>
         )
     }
 }
